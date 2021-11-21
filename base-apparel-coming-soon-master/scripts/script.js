f70d7fea8f25192f86378e4b86b5b423a8fc5766
Abdul400 <Abdulhafidhadan400@gmail.com>
2021-11-21 07:55:05 +0300

    made formatting changes using prettier

--- base-apparel-coming-soon-master/scripts/script.js
+++ base-apparel-coming-soon-master/scripts/script.js
@@ -1,92 +1,77 @@
 let myInput = document.getElementById('input'),
-    myBtn = document.getElementById('button'),
-    errorIcon = document.getElementById('error'),
-    warning = document.getElementById('warning');
+  myBtn = document.getElementById('button'),
+  errorIcon = document.getElementById('error'),
+  warning = document.getElementById('warning');
 
-    console.log(errorIcon)
-    
+console.log(errorIcon);
+
 window.onload = function () {
+  errorIcon.classList.remove('visible');
+  warning.classList.remove('visible');
 
-        errorIcon.classList.remove('visible');
-        warning.classList.remove('visible');
+  errorIcon.classList.add('invisible');
+  warning.classList.add('invisible');
+};
 
-        errorIcon.classList.add('invisible');
-        warning.classList.add('invisible');
-    }
+myInput.addEventListener('keyup', () => {
+  'use strict';
+  let text = myInput.value;
+  let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-myInput.addEventListener('keyup', ()=> {
-    'use strict'
-    let text = myInput.value
-    let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    
-    if (text === '') {
-        errorIcon.classList.remove('visible');
-        warning.classList.remove('visible');
+  if (text === '') {
+    errorIcon.classList.remove('visible');
+    warning.classList.remove('visible');
 
-        errorIcon.classList.add("invisible");
-        warning.classList.add("invisible");
-        } 
-    else if (!text.match(pattern)) {
+    errorIcon.classList.add('invisible');
+    warning.classList.add('invisible');
+  } else if (!text.match(pattern)) {
+    errorIcon.src = '/images/icon-error.svg';
+    warning.innerHTML = 'please provide a valid email';
+    warning.style.color = 'red';
 
-        errorIcon.src ='/images/icon-error.svg';
-        warning.innerHTML='please provide a valid email';
-        warning.style.color ='red'
+    errorIcon.classList.remove('invisible');
+    warning.classList.remove('invisible');
 
-        errorIcon.classList.remove("invisible");
-        warning.classList.remove("invisible");
+    errorIcon.classList.add('visible');
+    warning.classList.add('visible');
+  } else if (text.match(pattern)) {
+    errorIcon.src = '/images/check.png';
+    warning.innerHTML = 'Email is Valid';
+    warning.style.color = '#32BEA6';
 
-        errorIcon.classList.add('visible');
-        warning.classList.add('visible');
-        }
-    else if(text.match(pattern)){
-        errorIcon.src ='/images/check.png';
-        warning.innerHTML='Email is Valid';
-        warning.style.color ='#32BEA6'
+    errorIcon.classList.remove('invisible');
+    warning.classList.remove('invisible');
 
-        errorIcon.classList.remove("invisible");
-        warning.classList.remove("invisible");
+    errorIcon.classList.add('visible');
+    warning.classList.add('visible');
+  }
+});
 
-        errorIcon.classList.add('visible');
-        warning.classList.add('visible');
-    }
-})
+myBtn.addEventListener('click', () => {
+  'use strict';
+  let text = myInput.value;
+  let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-myBtn.addEventListener('click', ()=>{
-    'use strict'
+  if (text === '') {
+    window.alert('email field cannot be empty');
+  } else if (text.match(pattern)) {
+    location.href = 'page2.html';
+  } else if (!text.match(pattern)) {
+    window.alert('email is invalid. please provide a valid mail');
+  }
+});
+
+myInput.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
     let text = myInput.value;
     let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    
-    if (text === '') {
-        window.alert('email field cannot be empty')
-  
-        } 
-    else if (text.match(pattern)) {
-        location.href = "page2.html"
-    }
-    else if (!text.match(pattern)) {
-        window.alert('email is invalid. please provide a valid mail')
-        }
-    } 
-)
-   
-myInput.addEventListener('keyup', (event)=> {
-        if (event.keyCode === 13) {
-            let text = myInput.value;
-            let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            
-            if (text === '') {
-                window.alert('email field cannot be empty')
-          
-                } 
-            else if (text.match(pattern)) {
-                location.href = "page2.html"
-            }
-            else if (!text.match(pattern)) {
-                window.alert('email is invalid. please provide a valid mail')
-                }
-            } 
-        }
-    )
-    
 
-    
+    if (text === '') {
+      window.alert('email field cannot be empty');
+    } else if (text.match(pattern)) {
+      location.href = 'page2.html';
+    } else if (!text.match(pattern)) {
+      window.alert('email is invalid. please provide a valid mail');
+    }
+  }
+});
